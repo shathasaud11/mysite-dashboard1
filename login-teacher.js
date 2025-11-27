bef9beaf1ecf04dceb2d54712cd88d68115ff892
shathasaud11 <116908473+shathasaud11@users.noreply.github.com>
2025-11-27 15:45:40 +0300

    Update login-teacher.js

--- login-teacher.js
+++ login-teacher.js
@@ -1,1 +1,15 @@
+// كلمة السر للمعلمة
+const TEACHER_PASSWORD = "1234"; // يمكن تغييره لاحقاً
+
+function loginTeacher() {
+    const input = document.getElementById("teacherPassword").value;
+    const error = document.getElementById("errorMsg");
+
+    if (input === TEACHER_PASSWORD) {
+        localStorage.setItem("teacherLogged", "true");
+        window.location.href = "teacher-dashboard.html";
+    } else {
+        error.textContent = "❌ الرقم السري غير صحيح";
+    }
+}
 
